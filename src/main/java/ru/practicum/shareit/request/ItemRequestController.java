1e4c8d4a3a12afd4f72c6e97f50c5f1fad7002ec
Texpl1nII <Texpl1n@yandex.ru>
2025-09-19 21:31:04 +0500

    Удалил лишнее

--- src/main/java/ru/practicum/shareit/request/ItemRequestController.java
+++ src/main/java/ru/practicum/shareit/request/ItemRequestController.java
@@ -4,35 +4,34 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 import ru.practicum.shareit.request.dto.ItemRequestDto;
-import ru.practicum.shareit.request.dto.ItemRequestService;
 
 import jakarta.validation.Valid;
+import java.util.Collections;
 import java.util.List;
 
 @RestController
 @RequestMapping(path = "/requests")
 @RequiredArgsConstructor
 public class ItemRequestController {
-    private final ItemRequestService itemRequestService;
 
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
     public ItemRequestDto createItemRequest(@Valid @RequestBody ItemRequestDto itemRequestDto, @RequestHeader("X-Sharer-User-Id") Long userId) {
-        return itemRequestService.createItemRequest(itemRequestDto, userId);
+        return new ItemRequestDto();
     }
 
     @GetMapping
     public List<ItemRequestDto> getUserItemRequests(@RequestHeader("X-Sharer-User-Id") Long userId) {
-        return itemRequestService.getUserItemRequests(userId);
+        return Collections.emptyList();
     }
 
     @GetMapping("/all")
     public List<ItemRequestDto> getAllItemRequests(@RequestParam(defaultValue = "0") Integer from, @RequestParam(defaultValue = "10") Integer size, @RequestHeader("X-Sharer-User-Id") Long userId) {
-        return itemRequestService.getAllItemRequests(from, size, userId);
+        return Collections.emptyList();
     }
 
     @GetMapping("/{requestId}")
     public ItemRequestDto getItemRequestById(@PathVariable Long requestId, @RequestHeader("X-Sharer-User-Id") Long userId) {
-        return itemRequestService.getItemRequestById(requestId, userId);
+        return new ItemRequestDto();
     }
 }
